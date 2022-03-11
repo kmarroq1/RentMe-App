7a9df8528e7cafbecb849e407e8ec0ba886f3669
Spencer Dent <sd00070@my.westga.edu>
2022-03-11 12:49:16 -0500

    create basic Person models and their properties

---
+++ RentMe-App/Model/Member.cs
@@ -0,0 +1,9 @@
+﻿namespace RentMe_App.Model
+{
+    /// <summary>
+    /// Represents the data stored in a row in the Employee table in the database.
+    /// </summary>
+    public class Member : Person
+    {
+    }
+}
