1a9a2103a256a60df054e1f74dfa7e568e5bf801
Kimberly Marroquin <marroquin.kim3@gmail.com>
2022-03-27 19:47:16 -0400

    Created Employee Search user control wireframe

---
+++ RentMe-App/UserControls/SearchEmployeeUserControl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RentMe_App.UserControls
+{
+    public partial class SearchEmployeeUserControl : UserControl
+    {
+        public SearchEmployeeUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EditButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
