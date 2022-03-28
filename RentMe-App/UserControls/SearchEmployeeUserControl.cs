0c4492b615b1eac1b1989ce79456b62b10fb3667
Kimberly Marroquin <marroquin.kim3@gmail.com>
2022-03-27 20:04:39 -0400

    Added View folder and main forms

--- RentMe-App/UserControls/SearchEmployeeUserControl.cs
+++ RentMe-App/UserControls/SearchEmployeeUserControl.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RentMe_App.UserControls
@@ -24,7 +17,10 @@ namespace RentMe_App.UserControls
 
         private void ClearButton_Click(object sender, EventArgs e)
         {
-
+            fNameTextBox.Clear();
+            lNameTextBox.Clear();
+            idTextBox.Clear();
+            phoneTextBox.Clear();
         }
 
         private void EditButton_Click(object sender, EventArgs e)
