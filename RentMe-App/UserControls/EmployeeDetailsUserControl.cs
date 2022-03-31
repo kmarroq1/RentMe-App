c3c6a598dee3184a3fef11b7986ea9855cf4ac33
Spencer Dent <sd00070@my.westga.edu>
2022-03-31 01:51:21 -0400

    create & bind EmployeeDetailsUserControl

---
+++ RentMe-App/UserControls/EmployeeDetailsUserControl.cs
@@ -0,0 +1,18 @@
+﻿using RentMe_App.Model;
+using System.Windows.Forms;
+
+namespace RentMe_App.UserControls
+{
+    public partial class EmployeeDetailsUserControl : UserControl
+    {
+        public EmployeeDetailsUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public void FillDetails(Employee employeeToShow)
+        {
+            employeeBindingSource.DataSource = employeeToShow;
+        }
+    }
+}
