f81f488e82e8b04ce109da0fd98edbb5113040bc
Spencer Dent <sd00070@my.westga.edu>
2022-03-31 01:51:51 -0400

    create & bind MemberDetailsUserControl

---
+++ RentMe-App/UserControls/MemberDetailsUserControl.Designer.cs
@@ -0,0 +1,291 @@
+﻿
+namespace RentMe_App.UserControls
+{
+    partial class MemberDetailsUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label address1Label;
+            System.Windows.Forms.Label address2Label;
+            System.Windows.Forms.Label birthDateLabel;
+            System.Windows.Forms.Label cityLabel;
+            System.Windows.Forms.Label fnameLabel;
+            System.Windows.Forms.Label lnameLabel;
+            System.Windows.Forms.Label phoneLabel;
+            System.Windows.Forms.Label zipLabel;
+            System.Windows.Forms.Label stateLabel;
+            this.memberBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.activeCheckBox = new System.Windows.Forms.CheckBox();
+            this.address1TextBox = new System.Windows.Forms.TextBox();
+            this.address2TextBox = new System.Windows.Forms.TextBox();
+            this.birthDateDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.cityTextBox = new System.Windows.Forms.TextBox();
+            this.fnameTextBox = new System.Windows.Forms.TextBox();
+            this.lnameTextBox = new System.Windows.Forms.TextBox();
+            this.phoneTextBox = new System.Windows.Forms.TextBox();
+            this.zipTextBox = new System.Windows.Forms.TextBox();
+            this.stateComboBox = new System.Windows.Forms.ComboBox();
+            this.ErrorMessageLabel = new System.Windows.Forms.Label();
+            address1Label = new System.Windows.Forms.Label();
+            address2Label = new System.Windows.Forms.Label();
+            birthDateLabel = new System.Windows.Forms.Label();
+            cityLabel = new System.Windows.Forms.Label();
+            fnameLabel = new System.Windows.Forms.Label();
+            lnameLabel = new System.Windows.Forms.Label();
+            phoneLabel = new System.Windows.Forms.Label();
+            zipLabel = new System.Windows.Forms.Label();
+            stateLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.memberBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // memberBindingSource
+            // 
+            this.memberBindingSource.DataSource = typeof(RentMe_App.Model.Member);
+            // 
+            // activeCheckBox
+            // 
+            this.activeCheckBox.DataBindings.Add(new System.Windows.Forms.Binding("CheckState", this.memberBindingSource, "Active", true));
+            this.activeCheckBox.Location = new System.Drawing.Point(8, 211);
+            this.activeCheckBox.Name = "activeCheckBox";
+            this.activeCheckBox.Size = new System.Drawing.Size(200, 24);
+            this.activeCheckBox.TabIndex = 2;
+            this.activeCheckBox.Text = "Active";
+            this.activeCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // address1Label
+            // 
+            address1Label.AutoSize = true;
+            address1Label.Location = new System.Drawing.Point(5, 58);
+            address1Label.Name = "address1Label";
+            address1Label.Size = new System.Drawing.Size(54, 13);
+            address1Label.TabIndex = 3;
+            address1Label.Text = "Address1:";
+            // 
+            // address1TextBox
+            // 
+            this.address1TextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.memberBindingSource, "Address1", true));
+            this.address1TextBox.Location = new System.Drawing.Point(68, 55);
+            this.address1TextBox.Name = "address1TextBox";
+            this.address1TextBox.Size = new System.Drawing.Size(200, 20);
+            this.address1TextBox.TabIndex = 4;
+            // 
+            // address2Label
+            // 
+            address2Label.AutoSize = true;
+            address2Label.Location = new System.Drawing.Point(5, 84);
+            address2Label.Name = "address2Label";
+            address2Label.Size = new System.Drawing.Size(54, 13);
+            address2Label.TabIndex = 5;
+            address2Label.Text = "Address2:";
+            // 
+            // address2TextBox
+            // 
+            this.address2TextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.memberBindingSource, "Address2", true));
+            this.address2TextBox.Location = new System.Drawing.Point(68, 81);
+            this.address2TextBox.Name = "address2TextBox";
+            this.address2TextBox.Size = new System.Drawing.Size(200, 20);
+            this.address2TextBox.TabIndex = 6;
+            // 
+            // birthDateLabel
+            // 
+            birthDateLabel.AutoSize = true;
+            birthDateLabel.Location = new System.Drawing.Point(5, 189);
+            birthDateLabel.Name = "birthDateLabel";
+            birthDateLabel.Size = new System.Drawing.Size(57, 13);
+            birthDateLabel.TabIndex = 7;
+            birthDateLabel.Text = "Birth Date:";
+            // 
+            // birthDateDateTimePicker
+            // 
+            this.birthDateDateTimePicker.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.memberBindingSource, "BirthDate", true));
+            this.birthDateDateTimePicker.Location = new System.Drawing.Point(68, 185);
+            this.birthDateDateTimePicker.Name = "birthDateDateTimePicker";
+            this.birthDateDateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.birthDateDateTimePicker.TabIndex = 8;
+            // 
+            // cityLabel
+            // 
+            cityLabel.AutoSize = true;
+            cityLabel.Location = new System.Drawing.Point(5, 110);
+            cityLabel.Name = "cityLabel";
+            cityLabel.Size = new System.Drawing.Size(27, 13);
+            cityLabel.TabIndex = 9;
+            cityLabel.Text = "City:";
+            // 
+            // cityTextBox
+            // 
+            this.cityTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.memberBindingSource, "City", true));
+            this.cityTextBox.Location = new System.Drawing.Point(68, 107);
+            this.cityTextBox.Name = "cityTextBox";
+            this.cityTextBox.Size = new System.Drawing.Size(200, 20);
+            this.cityTextBox.TabIndex = 10;
+            // 
+            // fnameLabel
+            // 
+            fnameLabel.AutoSize = true;
+            fnameLabel.Location = new System.Drawing.Point(5, 6);
+            fnameLabel.Name = "fnameLabel";
+            fnameLabel.Size = new System.Drawing.Size(42, 13);
+            fnameLabel.TabIndex = 11;
+            fnameLabel.Text = "Fname:";
+            // 
+            // fnameTextBox
+            // 
+            this.fnameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.memberBindingSource, "Fname", true));
+            this.fnameTextBox.Location = new System.Drawing.Point(68, 3);
+            this.fnameTextBox.Name = "fnameTextBox";
+            this.fnameTextBox.Size = new System.Drawing.Size(200, 20);
+            this.fnameTextBox.TabIndex = 12;
+            // 
+            // lnameLabel
+            // 
+            lnameLabel.AutoSize = true;
+            lnameLabel.Location = new System.Drawing.Point(5, 32);
+            lnameLabel.Name = "lnameLabel";
+            lnameLabel.Size = new System.Drawing.Size(42, 13);
+            lnameLabel.TabIndex = 13;
+            lnameLabel.Text = "Lname:";
+            // 
+            // lnameTextBox
+            // 
+            this.lnameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.memberBindingSource, "Lname", true));
+            this.lnameTextBox.Location = new System.Drawing.Point(68, 29);
+            this.lnameTextBox.Name = "lnameTextBox";
+            this.lnameTextBox.Size = new System.Drawing.Size(200, 20);
+            this.lnameTextBox.TabIndex = 14;
+            // 
+            // phoneLabel
+            // 
+            phoneLabel.AutoSize = true;
+            phoneLabel.Location = new System.Drawing.Point(5, 162);
+            phoneLabel.Name = "phoneLabel";
+            phoneLabel.Size = new System.Drawing.Size(41, 13);
+            phoneLabel.TabIndex = 15;
+            phoneLabel.Text = "Phone:";
+            // 
+            // phoneTextBox
+            // 
+            this.phoneTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.memberBindingSource, "Phone", true));
+            this.phoneTextBox.Location = new System.Drawing.Point(68, 159);
+            this.phoneTextBox.Name = "phoneTextBox";
+            this.phoneTextBox.Size = new System.Drawing.Size(200, 20);
+            this.phoneTextBox.TabIndex = 16;
+            // 
+            // zipLabel
+            // 
+            zipLabel.AutoSize = true;
+            zipLabel.Location = new System.Drawing.Point(5, 136);
+            zipLabel.Name = "zipLabel";
+            zipLabel.Size = new System.Drawing.Size(25, 13);
+            zipLabel.TabIndex = 19;
+            zipLabel.Text = "Zip:";
+            // 
+            // zipTextBox
+            // 
+            this.zipTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.memberBindingSource, "Zip", true));
+            this.zipTextBox.Location = new System.Drawing.Point(68, 133);
+            this.zipTextBox.Name = "zipTextBox";
+            this.zipTextBox.Size = new System.Drawing.Size(200, 20);
+            this.zipTextBox.TabIndex = 20;
+            // 
+            // stateLabel
+            // 
+            stateLabel.AutoSize = true;
+            stateLabel.Location = new System.Drawing.Point(274, 110);
+            stateLabel.Name = "stateLabel";
+            stateLabel.Size = new System.Drawing.Size(35, 13);
+            stateLabel.TabIndex = 20;
+            stateLabel.Text = "State:";
+            // 
+            // stateComboBox
+            // 
+            this.stateComboBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.memberBindingSource, "State", true));
+            this.stateComboBox.FormattingEnabled = true;
+            this.stateComboBox.Location = new System.Drawing.Point(315, 107);
+            this.stateComboBox.Name = "stateComboBox";
+            this.stateComboBox.Size = new System.Drawing.Size(84, 21);
+            this.stateComboBox.TabIndex = 21;
+            // 
+            // ErrorMessageLabel
+            // 
+            this.ErrorMessageLabel.AutoSize = true;
+            this.ErrorMessageLabel.ForeColor = System.Drawing.Color.Red;
+            this.ErrorMessageLabel.Location = new System.Drawing.Point(133, 238);
+            this.ErrorMessageLabel.Name = "ErrorMessageLabel";
+            this.ErrorMessageLabel.Size = new System.Drawing.Size(75, 13);
+            this.ErrorMessageLabel.TabIndex = 22;
+            this.ErrorMessageLabel.Text = "Error Message";
+            // 
+            // AddMemberUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ErrorMessageLabel);
+            this.Controls.Add(stateLabel);
+            this.Controls.Add(this.stateComboBox);
+            this.Controls.Add(this.activeCheckBox);
+            this.Controls.Add(address1Label);
+            this.Controls.Add(this.address1TextBox);
+            this.Controls.Add(address2Label);
+            this.Controls.Add(this.address2TextBox);
+            this.Controls.Add(birthDateLabel);
+            this.Controls.Add(this.birthDateDateTimePicker);
+            this.Controls.Add(cityLabel);
+            this.Controls.Add(this.cityTextBox);
+            this.Controls.Add(fnameLabel);
+            this.Controls.Add(this.fnameTextBox);
+            this.Controls.Add(lnameLabel);
+            this.Controls.Add(this.lnameTextBox);
+            this.Controls.Add(phoneLabel);
+            this.Controls.Add(this.phoneTextBox);
+            this.Controls.Add(zipLabel);
+            this.Controls.Add(this.zipTextBox);
+            this.Name = "AddMemberUserControl";
+            this.Size = new System.Drawing.Size(404, 260);
+            ((System.ComponentModel.ISupportInitialize)(this.memberBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource memberBindingSource;
+        private System.Windows.Forms.CheckBox activeCheckBox;
+        private System.Windows.Forms.TextBox address1TextBox;
+        private System.Windows.Forms.TextBox address2TextBox;
+        private System.Windows.Forms.DateTimePicker birthDateDateTimePicker;
+        private System.Windows.Forms.TextBox cityTextBox;
+        private System.Windows.Forms.TextBox fnameTextBox;
+        private System.Windows.Forms.TextBox lnameTextBox;
+        private System.Windows.Forms.TextBox phoneTextBox;
+        private System.Windows.Forms.TextBox zipTextBox;
+        private System.Windows.Forms.ComboBox stateComboBox;
+        private System.Windows.Forms.Label ErrorMessageLabel;
+    }
+}
---
+++ RentMe-App/UserControls/MemberDetailsUserControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace RentMe_App.UserControls
+{
+    public partial class MemberDetailsUserControl : UserControl
+    {
+        public MemberDetailsUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
