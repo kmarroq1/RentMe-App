ce3d7f0aee4ec665d644f67bb8da224b339dd79c
Spencer Dent <sd00070@my.westga.edu>
2022-04-03 04:59:03 -0400

    create State DAL

---
+++ RentMe-App/DAL/StateDAL.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace RentMe_App.DAL
+{
+    /// <summary>
+    /// Data Access Layer which communicates with the States table in the db.
+    /// </summary>
+    public class StateDAL
+    {
+        /// <summary>
+        /// Collects the list of abbreviations from the states table.
+        /// </summary>
+        /// <returns>The abbreviations in the states table.</returns>
+        public List<string> GetStateAbbreviations()
+        {
+            List<string> abbreviations = new List<string>();
+
+            string selectStatement = @"SELECT [name]
+                                       FROM [states]";
+
+            using (SqlConnection connection = RentMeAppDBConnection.GetConnection())
+            {
+                connection.Open();
+
+                using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
+                    using(SqlDataReader reader = selectCommand.ExecuteReader())
+                        while(reader.Read())
+                            abbreviations.Add(reader.GetString(reader.GetOrdinal("name")));
+            }
+
+            return abbreviations;
+        }
+    }
+}
