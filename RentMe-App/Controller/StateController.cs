d0065205c9d690cdd094088f5aa1f1de0a0c9bd3
Spencer Dent <sd00070@my.westga.edu>
2022-04-03 05:58:07 -0400

    create StateController

---
+++ RentMe-App/Controller/StateController.cs
@@ -0,0 +1,30 @@
+﻿using RentMe_App.DAL;
+using System.Collections.Generic;
+
+namespace RentMe_App.Controller
+{
+    /// <summary>
+    /// Acts as a bridge bewteen the models, views, and StateDAL.
+    /// </summary>
+    public class StateController
+    {
+        private readonly StateDAL _stateData;
+        
+        /// <summary>
+        /// Initializes the StateDAL.
+        /// </summary>
+        public StateController()
+        {
+            _stateData = new StateDAL();
+        }
+
+        /// <summary>
+        /// Delegates to the StateDAL to collect the State abbreviations from the db.
+        /// </summary>
+        /// <returns>The State abbreviations in the states table.</returns>
+        public List<string> GetStateAbbreviations()
+        {
+            return _stateData.GetStateAbbreviations();
+        }
+    }
+}
