5f8900a9e48db2e8b9dca81d756ce6c39b3508fe
Spencer Dent <sd00070@my.westga.edu>
2022-04-03 15:18:52 -0400

    gut DataSource to fix call stack avoidance error

--- RentMe-App/UserControls/EmployeeDashboardUCs/MemberDetailsUserControl.Designer.cs
+++ RentMe-App/UserControls/EmployeeDashboardUCs/MemberDetailsUserControl.Designer.cs
@@ -29,7 +29,6 @@ namespace RentMe_App.UserControls
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.address1Label = new System.Windows.Forms.Label();
             this.address2Label = new System.Windows.Forms.Label();
             this.birthDateLabel = new System.Windows.Forms.Label();
@@ -40,7 +39,6 @@ namespace RentMe_App.UserControls
             this.zipLabel = new System.Windows.Forms.Label();
             this.stateLabel = new System.Windows.Forms.Label();
             this.activeCheckBox = new System.Windows.Forms.CheckBox();
-            this.memberBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.address1TextBox = new System.Windows.Forms.TextBox();
             this.address2TextBox = new System.Windows.Forms.TextBox();
             this.birthDateDateTimePicker = new System.Windows.Forms.DateTimePicker();
@@ -50,7 +48,6 @@ namespace RentMe_App.UserControls
             this.phoneTextBox = new System.Windows.Forms.TextBox();
             this.zipTextBox = new System.Windows.Forms.TextBox();
             this.stateComboBox = new System.Windows.Forms.ComboBox();
-            ((System.ComponentModel.ISupportInitialize)(this.memberBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // address1Label
@@ -136,7 +133,6 @@ namespace RentMe_App.UserControls
             // 
             // activeCheckBox
             // 
-            this.activeCheckBox.DataBindings.Add(new System.Windows.Forms.Binding("CheckState", this.memberBindingSource, "Active", true));
             this.activeCheckBox.Location = new System.Drawing.Point(8, 211);
             this.activeCheckBox.Name = "activeCheckBox";
             this.activeCheckBox.Size = new System.Drawing.Size(200, 24);
@@ -144,13 +140,8 @@ namespace RentMe_App.UserControls
             this.activeCheckBox.Text = "Active";
             this.activeCheckBox.UseVisualStyleBackColor = true;
             // 
-            // memberBindingSource
-            // 
-            this.memberBindingSource.DataSource = typeof(RentMe_App.Model.Member);
-            // 
             // address1TextBox
             // 
-            this.address1TextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.memberBindingSource, "Address1", true));
             this.address1TextBox.Location = new System.Drawing.Point(68, 55);
             this.address1TextBox.Name = "address1TextBox";
             this.address1TextBox.Size = new System.Drawing.Size(200, 20);
@@ -158,7 +149,6 @@ namespace RentMe_App.UserControls
             // 
             // address2TextBox
             // 
-            this.address2TextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.memberBindingSource, "Address2", true));
             this.address2TextBox.Location = new System.Drawing.Point(68, 81);
             this.address2TextBox.Name = "address2TextBox";
             this.address2TextBox.Size = new System.Drawing.Size(200, 20);
@@ -166,7 +156,6 @@ namespace RentMe_App.UserControls
             // 
             // birthDateDateTimePicker
             // 
-            this.birthDateDateTimePicker.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.memberBindingSource, "BirthDate", true));
             this.birthDateDateTimePicker.Location = new System.Drawing.Point(68, 185);
             this.birthDateDateTimePicker.Name = "birthDateDateTimePicker";
             this.birthDateDateTimePicker.Size = new System.Drawing.Size(200, 20);
@@ -174,7 +163,6 @@ namespace RentMe_App.UserControls
             // 
             // cityTextBox
             // 
-            this.cityTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.memberBindingSource, "City", true));
             this.cityTextBox.Location = new System.Drawing.Point(68, 107);
             this.cityTextBox.Name = "cityTextBox";
             this.cityTextBox.Size = new System.Drawing.Size(200, 20);
@@ -182,7 +170,6 @@ namespace RentMe_App.UserControls
             // 
             // fnameTextBox
             // 
-            this.fnameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.memberBindingSource, "Fname", true));
             this.fnameTextBox.Location = new System.Drawing.Point(68, 3);
             this.fnameTextBox.Name = "fnameTextBox";
             this.fnameTextBox.Size = new System.Drawing.Size(200, 20);
@@ -190,7 +177,6 @@ namespace RentMe_App.UserControls
             // 
             // lnameTextBox
             // 
-            this.lnameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.memberBindingSource, "Lname", true));
             this.lnameTextBox.Location = new System.Drawing.Point(68, 29);
             this.lnameTextBox.Name = "lnameTextBox";
             this.lnameTextBox.Size = new System.Drawing.Size(200, 20);
@@ -198,7 +184,6 @@ namespace RentMe_App.UserControls
             // 
             // phoneTextBox
             // 
-            this.phoneTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.memberBindingSource, "Phone", true));
             this.phoneTextBox.Location = new System.Drawing.Point(68, 159);
             this.phoneTextBox.Name = "phoneTextBox";
             this.phoneTextBox.Size = new System.Drawing.Size(200, 20);
@@ -206,7 +191,6 @@ namespace RentMe_App.UserControls
             // 
             // zipTextBox
             // 
-            this.zipTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.memberBindingSource, "Zip", true));
             this.zipTextBox.Location = new System.Drawing.Point(68, 133);
             this.zipTextBox.Name = "zipTextBox";
             this.zipTextBox.Size = new System.Drawing.Size(200, 20);
@@ -214,7 +198,6 @@ namespace RentMe_App.UserControls
             // 
             // stateComboBox
             // 
-            this.stateComboBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.memberBindingSource, "State", true));
             this.stateComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.stateComboBox.FormattingEnabled = true;
             this.stateComboBox.Location = new System.Drawing.Point(315, 107);
@@ -247,16 +230,12 @@ namespace RentMe_App.UserControls
             this.Controls.Add(this.zipTextBox);
             this.Name = "MemberDetailsUserControl";
             this.Size = new System.Drawing.Size(404, 242);
-            this.Load += new System.EventHandler(this.MemberDetailsUserControl_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.memberBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.BindingSource memberBindingSource;
         private System.Windows.Forms.CheckBox activeCheckBox;
         private System.Windows.Forms.TextBox address1TextBox;
         private System.Windows.Forms.TextBox address2TextBox;
--- RentMe-App/UserControls/EmployeeDashboardUCs/MemberDetailsUserControl.cs
+++ RentMe-App/UserControls/EmployeeDashboardUCs/MemberDetailsUserControl.cs
@@ -10,26 +10,11 @@ namespace RentMe_App.UserControls
     /// </summary>
     public partial class MemberDetailsUserControl : UserControl
     {
+        #region Fields
         private readonly StateController _stateController;
+        #endregion
 
-        /// <summary>
-        /// Initializes UC's components & controller.
-        /// </summary>
-        public MemberDetailsUserControl()
-        {
-            InitializeComponent();
-            _stateController = new StateController();
-        }
-
-        /// <summary>
-        /// Clears all of the fields and resets the birth date field.
-        /// </summary>
-        public void ClearFields()
-        {
-            memberBindingSource.Clear();
-            birthDateDateTimePicker.Value = DateTime.Now;
-        }
-
+        #region Properties
         /// <summary>
         /// Dictates how the implicitly stored member is accessed and mutated.
         /// </summary>
@@ -38,28 +23,68 @@ namespace RentMe_App.UserControls
             get
             {
                 Member newMember = new Member
-                { Fname = fnameTextBox.Text
-                , Lname = lnameTextBox.Text
-                , BirthDate = birthDateDateTimePicker.Value
-                , Phone = phoneTextBox.Text
-                , Active = activeCheckBox.Checked
-                , Address1 = address1TextBox.Text
-                , City = cityTextBox.Text
-                , State = stateComboBox.SelectedItem.ToString()
-                , Zip = zipTextBox.Text
+                {
+                    Fname = fnameTextBox.Text,
+                    Lname = lnameTextBox.Text,
+                    BirthDate = birthDateDateTimePicker.Value,
+                    Phone = phoneTextBox.Text,
+                    Active = activeCheckBox.Checked,
+                    Address1 = address1TextBox.Text,
+                    City = cityTextBox.Text,
+                    Zip = zipTextBox.Text
                 };
+
+                if (stateComboBox.SelectedIndex > -1)
+                    newMember.State = stateComboBox.SelectedItem.ToString();
 
                 if (!string.IsNullOrWhiteSpace(address2TextBox.Text))
                     newMember.Address2 = address2TextBox.Text;
 
                 return newMember;
             }
-            set => memberBindingSource.DataSource = value;
+            set
+            {
+                fnameTextBox.Text = value.Fname;
+                lnameTextBox.Text = value.Lname;
+                birthDateDateTimePicker.Value = value.BirthDate;
+                phoneTextBox.Text = value.Phone;
+                activeCheckBox.Checked = value.Active;
+                address1TextBox.Text = value.Address1;
+                address2TextBox.Text = value.Address2;
+                cityTextBox.Text = value.City;
+                stateComboBox.SelectedIndex = stateComboBox.FindStringExact(value.State);
+                zipTextBox.Text = value.Zip;
+            }
         }
+        #endregion
 
-        private void MemberDetailsUserControl_Load(object sender, EventArgs e)
+        #region Constructors
+        /// <summary>
+        /// Initializes UC's components & controller.
+        /// </summary>
+        public MemberDetailsUserControl()
         {
+            InitializeComponent();
+            _stateController = new StateController();
+
+            stateComboBox.Items.Clear();
             stateComboBox.Items.AddRange(_stateController.GetStateAbbreviations().ToArray());
         }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Clears all of the fields and resets the birth date field.
+        /// </summary>
+        public void ClearFields()
+        {
+            foreach (TextBox textBox in new TextBox[] { fnameTextBox, lnameTextBox, phoneTextBox, address1TextBox, address2TextBox, cityTextBox, zipTextBox })
+                textBox.Clear();
+
+            birthDateDateTimePicker.Value = new DateTime(1970, 1, 1, 0, 0, 0);
+            activeCheckBox.Checked = false;
+            stateComboBox.SelectedIndex = -1;
+        }
+        #endregion
     }
 }
