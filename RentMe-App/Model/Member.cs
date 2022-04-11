0aad6282defc813e2e88f127a44632e66b53e938
Spencer Dent <sd00070@my.westga.edu>
2022-04-11 10:27:52 -0400

    tweak member field format error msgs

--- RentMe-App/Model/Member.cs
+++ RentMe-App/Model/Member.cs
@@ -106,7 +106,7 @@ namespace RentMe_App.Model
 
                 Regex phoneRegex = new Regex(@"(^\(\d{3}\) ?\d{3}-\d{4}$)|(^\d{10}$)|(^\d{3}[.-]\d{3}[.-]\d{4}$)");
                 if (!phoneRegex.IsMatch(value))
-                    throw new ArgumentException("Phone is not in valid format.");
+                    throw new ArgumentException("Phone is not in valid format.\nPlease use either (###) ###-#### or ###-###-####.");
 
                 string storedFormat = "";
 
@@ -202,7 +202,7 @@ namespace RentMe_App.Model
                 Regex zipCodeRegex = new Regex(@"(^\d{5}$)|(^\d{5}-\d{4}$)");
 
                 if (!zipCodeRegex.IsMatch(value))
-                    throw new ArgumentException("Zip code not in valid format.");
+                    throw new ArgumentException("Zip code not in valid format.\nPlease use either ##### or #####-####.");
 
                 _zip = value;
             }
