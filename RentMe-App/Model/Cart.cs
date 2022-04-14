78b914588229722b1921c0cd8c2380694eca0026
Spencer Dent <sd00070@my.westga.edu>
2022-04-13 23:27:37 -0400

    add convenience methods

--- RentMe-App/Model/Cart.cs
+++ RentMe-App/Model/Cart.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace RentMe_App.Model
+﻿namespace RentMe_App.Model
 {
     /// <summary>
     /// A global static Cart which can be accessed from anywhere in the application.
@@ -18,6 +16,20 @@ namespace RentMe_App.Model
         /// Obtains the Return Transaction from the cart.
         /// </summary>
         public static Return Return { get; set; }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Returns whether or not the Cart contains a Rental Transaction.
+        /// </summary>
+        /// <returns>True if is has a Rental Transation; false otherwise.</returns>
+        public static bool HasRental() { return Rental != null; }
+
+        /// <summary>
+        /// Returns whether of not the Cart contains a Return Transaction.
+        /// </summary>
+        /// <returns>True if it has a Return Transaction; false otherwise.</returns>
+        public static bool HasReturn() { return Return != null; }
         #endregion
     }
 }
