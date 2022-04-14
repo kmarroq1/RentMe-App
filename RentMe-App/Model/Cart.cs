de9983b53f950c390974859e477e734b6f533b1a
Spencer Dent <sd00070@my.westga.edu>
2022-04-13 22:12:18 -0400

    fill-out the static Cart class
    > still needs to be tested...

--- RentMe-App/Model/Cart.cs
+++ RentMe-App/Model/Cart.cs
@@ -2,9 +2,47 @@
 
 namespace RentMe_App.Model
 {
+    /// <summary>
+    /// A global static Cart which can be accessed from anywhere in the application.
+    /// We really should look into making this a Singleton.
+    /// </summary>
     public static class Cart
     {
-        private static List<Rental> _rentals;
-        private static List<Return> _returns;
+        #region Fields
+        private static readonly List<Rental> _rentals = new List<Rental>();
+        private static readonly List<Return> _returns = new List<Return>();
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Obtains the list of Rentals from the cart.
+        /// </summary>
+        public static List<Rental> Rentals { get => _rentals; }
+
+        /// <summary>
+        /// Obtains the list of Returns from the cart.
+        /// </summary>
+        public static List<Return> Returns { get => _returns; }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Add a new Rental to the Rentals portion of the cart.
+        /// </summary>
+        /// <param name="newRental">The new Rental</param>
+        public static void AddRental(Rental newRental)
+        {
+            _rentals.Add(newRental);
+        }
+
+        /// <summary>
+        /// Add a new Return to the Returns portion of the cart.
+        /// </summary>
+        /// <param name="newReturn">The new Return</param>
+        public static void AddReturn(Return newReturn)
+        {
+            _returns.Add(newReturn);
+        }
+        #endregion
     }
 }
